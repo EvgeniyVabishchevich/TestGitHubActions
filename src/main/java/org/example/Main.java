c6c95d4863057f0f2852b4a23dbbe46a88b9b367
Evgeniy Vabishchevich <evgeniy.vabishevich.99@mail.ru>
2023-08-05 21:14:57 +0300

    commmit m

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -4,6 +4,7 @@ public class Main {
     public static void main(String[] args) {
         System.out.println("Hello world!");
         System.out.println("hellloooo");
+        System.out.println("heellliii");
         System.out.println("hello testing");
     }
 }
